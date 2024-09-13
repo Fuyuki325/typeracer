82d396d449cbb4851e6ff83fe233d03bc5800b3f
Fuyuki Malahom <fuyuki325@gmail.com>
2024-09-13 07:17:20 -0700

    done

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import keyboard
+import requests
 from bs4 import BeautifulSoup
 
+def openTypeRacer():
+    browser = webdriver.Firefox()
+    browser.get('https://play.typeracer.com/')
 
-html = """
-<div class="product">
-  <h2>Product Title</h2>
-  <div class="price">
-    <span class="discount">12.99</span>
-    <span class="full">19.99</span>
-  </div>
-</div>
-"""
+    # Wait for and click the "Enter a Typing Race" button
+    enter_race_button = WebDriverWait(browser, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, ".gwt-Anchor.prompt-button.bkgnd-green"))
+    )
+    enter_race_button.click()
+
+    # Allow some extra time for all dynamic content to load
+    time.sleep(5)
+    inputElem = findInput(browser)
+    text = parsing(browser)
+    keyboard.wait('ctrl')
+    typing(inputElem, text)
 
 
 
-soup = BeautifulSoup(html, 'html.parser')
+def findInput(browser):
+  # Wait for and click the "Enter a Typing Race" button
+  inputElem = browser.find_element(By.TAG_NAME, 'input')
+  
+  return inputElem
+
+def parsing(browser):
+  # Find all relevant elements that contain the text to type
+  spans = browser.find_elements(By.CSS_SELECTOR, "span[unselectable='on']")
+  text = ""
+
+  span_count = len(spans)
+  first_span = True
+
+  print(span_count)
+        
+  for span in spans:
+    span_text = span.text.strip()
+    if span_count == 2:
+      if first_span:
+        text += span_text + ' '
+        first_span = False
+      else:
+        text += span_text 
+    elif span_count == 3:
+      if first_span:
+        text += span_text
+        first_span = False
+      else:
+        text += span_text + ' '
+
+  text = text.strip()
+
+  return text
+
+def typing(inputElem, text):
+  for c in text:
+    inputElem.send_keys(c)
+    time.sleep(0.1)
 
 
-product = {
-    "title": soup.find("span").text,
-}
-
-print(product)
+openTypeRacer()
